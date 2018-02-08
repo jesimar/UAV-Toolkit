7fa3ea6c3303265fc9425e590076c7543b57b2ce
jesimar <jesimar.arantes@gmail.com>
2018-02-08 11:50:58 -0200

    removendo exec-fgfs.sh

--- Scripts/exec-fgfs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/games/fgfs --fg-root=/usr/share/games/flightgear --fg-scenery=/usr/share/games/flightgear/Scenery --airport=KSFO --aircraft=Rascal110-JSBSim --disable-random-objects --prop:/sim/rendering/random-vegetation=false --disable-panel --disable-hud-3d --enable-horizon-effect --enable-enhanced-lighting --disable-ai-models --disable-ai-traffic --enable-real-weather-fetch --enable-fullscreen --prop:/sim/rendering/texture-compression=off --geometry=1280x1024 --bpp=32 --disable-terrasync --disable-fgcom
