ac867f13de6a15efd3b5ed35e9598119db3942fe
jesimar <jesimar.arantes@gmail.com>
2018-02-21 16:57:19 -0300

    Alteração código do my-ip.sh

--- Scripts/my-ip.sh
+++ Scripts/my-ip.sh
@@ -4,4 +4,4 @@
 
 echo 'my ip:'
 
-ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'
+ip addr | grep 'BROADCAST,MULTICAST,UP,LOWER_UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'
