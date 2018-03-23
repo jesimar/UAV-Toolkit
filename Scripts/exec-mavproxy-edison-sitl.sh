fc4b2e8ff461192dc7bd5d7bc4ca39101c494038
jesimar <jesimar.arantes@gmail.com>
2018-03-23 15:11:13 -0300

    Update script mavproxy

--- Scripts/exec-mavproxy-edison-sitl.sh
+++ Scripts/exec-mavproxy-edison-sitl.sh
@@ -5,8 +5,8 @@
 #Description: Script that runs MAVProxy software on Intel Edison for SITL testing at Edison.
 #Descrição: Script que executa o software MAVProxy na Intel Edison para testes SITL na Edison.
 
-IP_GCS=192.168.43.124
-#IP_GCS=192.168.205.231
+#IP_GCS=192.168.43.124
+IP_GCS=192.168.205.231
 
 IP_EDISON=127.0.0.1
 
