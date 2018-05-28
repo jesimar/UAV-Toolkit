6585f4bbf8f4831eb5b02acd482edbe7198c75dd
jesimar <jesimar.arantes@gmail.com>
2018-05-28 16:50:10 -0300

    removendo alguns projetos antigos

--- UAV-Voice-Commands/exec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Author: Jesimar da Silva Arantes
-#Date: 18/01/2018
-#Last Update: 18/01/2018
-#Description: Script that runs the UAV-Voice-Commands software.
-#Descrição: Script que executa o software UAV-Voice-Commands.
-
-/home/jesimar/jdk1.8.0_151/bin/java -jar dist/UAV-Voice-Commands.jar
